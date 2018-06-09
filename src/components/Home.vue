ce04ef6c8a6a13fb3b7ed20c4f77cec9635fd22d
vimm0 <vimmrana0@gmail.com>
2018-06-09 22:19:35 +0545

    Add v-for in StackLayout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,44 @@
 <template>
     <Page class="page">
     <!--<WebView src="~/Network.html" />-->
+            <ScrollView style="height: 100%">
+
     <StackLayout>
     <Image src="http://jus-static-prod.s3-website-us-east-1.amazonaws.com/static/img/home-hero-bg.5fb3abf.jpeg" class="cover"/>
-        <DockLayout orientation="horizontal" horizontalAlignment="center" verticalAlignment="center" minHeight="100">
-            <Image src="http://jusbroadcasting.com/jus-punjabi/wp-content/uploads/2016/09/jus-punjabi.png" width="50" height="50" class="network" stretch="none"/>
-            <Image src="http://jusbroadcasting.com/jus-punjabi/wp-content/uploads/2016/09/jus24x7.png" width="50" height="50" class="network" stretch="none"/>
-            <Image src="http://jusbroadcasting.com/jus-punjabi/wp-content/uploads/2016/09/jus-one.png" width="50" height="50" class="network" stretch="none"/>
-            <Image src="http://jusbroadcasting.com/jus-punjabi/wp-content/uploads/2016/09/9xtashan.png" width="50" height="50" class="network" stretch="none"/>
-            <Image src="http://jusbroadcasting.com/jus-punjabi/wp-content/uploads/2016/09/jus-radio.png" width="50" height="50" class="network" stretch="none"/>
-        </DockLayout>
+       
+    <StackLayout class="container">
+        <StackLayout class="card" v-for="(item, index) in results" :key="index">
+            <Image :src="item.pic" width="100" height="100" stretch="aspectFill" row="0" col="0" />
+            <Label :text="item.title" /> 
+            <Label :text="item.duration" /> 
+            <Label :text="item.created" /> 
+        </StackLayout>             
     </StackLayout>
+    <DockLayout orientation="horizontal" horizontalAlignment="center" verticalAlignment="center" minHeight="100">
+        <Image src="http://jusbroadcasting.com/jus-punjabi/wp-content/uploads/2016/09/jus-punjabi.png" width="50" height="50" class="network" stretch="none"/>
+        <Image src="http://jusbroadcasting.com/jus-punjabi/wp-content/uploads/2016/09/jus24x7.png" width="50" height="50" class="network" stretch="none"/>
+        <Image src="http://jusbroadcasting.com/jus-punjabi/wp-content/uploads/2016/09/jus-one.png" width="50" height="50" class="network" stretch="none"/>
+        <Image src="http://jusbroadcasting.com/jus-punjabi/wp-content/uploads/2016/09/9xtashan.png" width="50" height="50" class="network" stretch="none"/>
+        <Image src="http://jusbroadcasting.com/jus-punjabi/wp-content/uploads/2016/09/jus-radio.png" width="50" height="50" class="network" stretch="none"/>
+    </DockLayout>
+
+    </StackLayout>
+            </ScrollView>
     </Page>
 </template>
 <script>
     import * as webViewModule from "tns-core-modules/ui/web-view";
+    // import Collection from './Collection'
+    import axios from 'axios';
+
     export default {
         data() {
             return {
-                // src: 'http://docs.nativescript.org'
+                results: '',
+                 countries: [
+        { name: "Australia", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/au.png" },
+        { name: "Belgium", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/be.png" },
+      ],
             }
         },
         methods: {
@@ -29,26 +49,20 @@
                 } else {
                     message = "Error loading " + args.url + ": " + args.error;
                 }
-            }
+            },
+        onItemTap: function (args) {
+      console.log('Item with index: ' + args.index + ' tapped');
+    },
         },
-        created() {
-            // setTimeout(() => {
-            // // let page = this.$refs.page.nativeView;
-            // // let webView = page.getViewById('webView');
-            // console.log("webViewModule");
-            // console.log(webViewModule);
-            // }, 1);
-            
-            // webView.on(webViewModule.android.getSettings().setDomStorageEnabled(true), function (webViewModule) {
-            //  let message;
-            //  if (!args.error) {
-            //           message = "WebView finished loading " + args.url;
-            //    }
-            //    else {
-            //     message = "Error loading " + args.url + ": " + args.error;
-            //    }
-            //  });
-            // webView.on(webViewModule.android.getSettings().setDomStorageEnabled(true))
+        mounted() {
+        const Vue = require('nativescript-vue/dist/index');
+        axios.get(`http://52.202.70.246/v1/video/?page_size=0`).then(
+        ({data}) => {
+            this.results = data
+            Vue.set(this.$data, 'results', data)
+            console.log(data)
+        }
+      )
         }
     }
 </script>
---
+++ src/components/Network.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="our-network home-network-bg">
+    <div class='container has-text-centered network-container'>
+      <h1 class="is-size-3 has-text-weight-bold m-2 network-title is-size-5-touch is-size-5-mobile">Our Network</h1>
+      <div class="sites m-2 has-text-centered-touch">
+        <nav class="level">
+          <div class="level-item">
+            <div class="m-2">
+              <a target="_blank" href="http://jusbroadcasting.com/jus-punjabi/">
+                <img src="http://jusbroadcasting.com/jus-punjabi/wp-content/uploads/2016/09/jus-punjabi.png" alt="Our Network">
+              </a>
+            </div>
+          </div>
+          <div class="level-item">
+            <div class="m-2">
+              <a target="_blank" href="http://jus24x7.com/">
+                <img src="http://jusbroadcasting.com/jus-punjabi/wp-content/uploads/2016/09/jus24x7.png" alt="Our Network">
+              </a>
+            </div>
+          </div>
+          <div class="level-item">
+            <div class="m-2">
+              <a target="_blank" href="http://jusbroadcasting.com/jus-one/">
+                <img src="http://jusbroadcasting.com/jus-punjabi/wp-content/uploads/2016/09/jus-one.png" alt="Our Network">
+              </a>
+            </div>
+          </div>
+          <div class="level-item">
+            <div class="m-2">
+              <a target="_blank" href="http://9xtashan.in/">
+                <img src="http://jusbroadcasting.com/jus-punjabi/wp-content/uploads/2016/09/9xtashan.png" alt="Our Network">
+              </a>
+            </div>
+          </div>
+          <div class="level-item">
+            <div class="m-2">
+              <a target="_blank" href="http://jusbroadcasting.com/jus-radio/">
+                <img src="http://jusbroadcasting.com/jus-punjabi/wp-content/uploads/2016/09/jus-radio.png" alt="Our Network">
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Network'
+}
+</script>
